27c08c80482a8eef18c862177aa1e1499a3f8405
Dalyborec <87717913+Dalyborec@users.noreply.github.com>
2022-01-16 15:20:46 +0100

    sdsdsd
    sdds

---
+++ UTB.Eshop.Web/Areas/Customer/Controllers/ComplainController.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using UTB.Eshop.Web.Models.Database;
+using UTB.Eshop.Web.Models.Entity;
+using UTB.Eshop.Web.Models.Identity;
+using System;
+
+namespace UTB.Eshop.Web.Areas.Customer.Controllers
+{
+
+    [Area("Customer")]
+    [Authorize(Roles = nameof(Roles.Customer))]
+    public class ComplainController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        readonly EshopDbContext eshopDbContext;
+
+
+        public ComplainController(EshopDbContext eshopDB)
+        {
+            eshopDbContext = eshopDB;
+        }
+
+
+        [HttpGet]
+        public IActionResult ComplainIt(int productID)
+        {
+            ViewData["ProductID"] = productID;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ComplainIt(Complain complaint)
+        {
+            eshopDbContext.Complaints.Add(complaint);
+            await eshopDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(CustomerOrdersController.Index),
+                nameof(CustomerOrdersController).Replace("Controller", String.Empty),
+                new { area = "Customer" });
+        }
+
+
+        public IActionResult Complain()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ UTB.Eshop.Web/Models/Entity/Complain.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace UTB.Eshop.Web.Models.Entity
+{
+    [Table(nameof(Complain))]
+    public class Complain
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        // [StringLength(50)]
+        [Required]
+        public int ProductID { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Reason { get; set; }
+
+
+        /* // new
+         [StringLength(3)]
+
+         public string isDigital { get; set; }
+
+         // Bylo zboží vráceno včas
+         [StringLength(50)]
+
+         public string ManagerNote { get; set; }*/
+    }
+}
+
+
+
